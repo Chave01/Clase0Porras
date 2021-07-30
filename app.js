d188ccc32129a07d42ce1327bbdb2b2439bb9c23
Chavella Porras <chavellaporras@Chavellas-MacBook-Pro.local>
2021-07-30 16:06:49 -0300

    se creo mi primer archivo java

---
+++ app.js
@@ -0,0 +1,6 @@
+ let nombreingresado = prompt ("Ingresar nombre")
+ let apellidoingresado = prompt ("Ingresar apellido")
+ let salida = "Bienvenido" +"  " + nombreingresado + " " + apellidoingresado;
+
+ alert (salida)
+
